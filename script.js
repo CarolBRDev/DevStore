90b22010fd6a986b30090f7170e0820e31f3269b
Caroline Ramos <carolbeatrice@gmail.com>
2024-04-24 15:15:16 -0300

    Criação de divs para os produtos e randerização dos produtos dentro delas

--- script.js
+++ script.js
@@ -6,42 +6,36 @@ fetch(URL)
         // Manipular os dados recebidos, por exemplo:
         const products = data.results;//produtos vai guardar o resultado da busca da api
 
-        // Criar lista de produtos HTML dinamicamente
-        const productList = document.createElement('ul');//cria um tag ul no html - lista
-        products.forEach(product => {//para cada item da lista
-            const listItem = document.createElement('li');//cria uma linha
+        //Selecionar elemento onde os produtos seráo renderizados
+        const productListContainer = document.getElementById('product-list');
 
-            //Cria a imagem do produto
+        //criar uma div para cada produto e renderizar os produtos dentro delas
+        products.forEach(product => {//para cada item da lista
+            //criar uma div para o produto
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('product'); //adiciona a div criada em uma classe para facilitar estilização por css
+
+            //criar a imagem do produto
             const image = document.createElement('img');//cria a tag img
             image.src = product.thumbnail;//url da imagem
             image.alt = product.title;//texto alternativo da imagem
-            listItem.appendChild(image);//insere a imagem dentro do item da lista
-             
+            productDiv.appendChild(image);//insere a imagem dentro da div criada            
 
             //Cria o título do produto
             const title = document.createElement('span');//cria tag span para colocar a informação escrita
             title.textContent = product.title;//coloca um conteudo, o titulo do produto, dentro do span
-            listItem.appendChild(title);//insere o titulo dentro do item da lista
+            productDiv.appendChild(title);//insere o titulo na div criada
             
 
             //Cria o preço do produto
             const price = document.createElement('span');//cria uma tag span
             price.textContent = ' - R$ ' + product.price.toFixed(2); //coloca o preço formatado
-            listItem.appendChild(price);//insere o preço no item da lista
+            productDiv.appendChild(price);//insere o preço na div criada
         
-        
-        
-        //// Criar elementos HTML dinamicamente (para poder usar o elemento criado para trazer mais de uma informação, precisa criar tags span e ai atribuir as informações como titulo, preço e tumbnail dentro de cada li)
-        // const productList = document.createElement('ul');//cria um tag ul no html
-        // products.forEach(product => {//para cada item da lista
-        //     const listItem = document.createElement('li');//criar linha da lista
-        //     listItem.textContent = product.title;//coloca o titulo do produto nesta linha
-        
-        productList.appendChild(listItem);//adiciona o listITem a productList criada anteriormente
+            // Inserir a div no contâiner principal
+            productListContainer.appendChild(productDiv);
         });
-
-        // Inserir na página HTML
-        document.getElementById('product-list').appendChild(productList);//coloca a productList dentro da div com id product-list
+        documento.getElementById('product-list').appendChild(productList);
     })
     .catch(error => {
         console.error('Erro ao obter os dados:', error);
